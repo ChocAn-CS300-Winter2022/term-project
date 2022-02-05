4af28b3829fa8c94d5a68fc6aaf78d66ff2c9016
Alana Gilston <abluescarab@users.noreply.github.com>
2022-02-04 22:59:04 -0800

    Person.id back to string because of leading zeroes

--- chocan/person.py
+++ chocan/person.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 
 class Person:
-    def __init__(self, id=0, name="", address="", city="", state="",
-        zip_code=0):
+    def __init__(self, id="", name="", address="", city="", state="",
+        zip_code=""):
         """Create a new instance of Person."""
-        self.id = int(str(id)[:9])
+        self.id = id[:9]
         self.name = name[:25]
         self.address = address[:25]
         self.city = city[:14]
         self.state = state[:2]
-        self.zip_code = int(str(zip_code)[:5])
+        self.zip_code = zip_code[:5]
 
     def display(self):
         """Display the person on the command line."""
@@ -30,9 +30,9 @@ class Person:
         with open(file, "w") as f:
             json.dump(self.__dict__, f, indent=4, sort_keys=False)
 
-    def load(self, id=0):
+    def load(self, id=""):
         """Load the person from disk with a given ID."""
-        if id == 0:
+        if id == "":
             id = self.id
 
         file = self.get_file(id)
@@ -47,9 +47,9 @@ class Person:
     @staticmethod
     def get_file(id):
         """Get the file that the person is stored in."""
-        if str(id).startswith("8"):
+        if id.startswith("8"):
             folder = "providers"
-        elif str(id).startswith("9"):
+        elif id.startswith("9"):
             folder = "managers"
         else:
             folder = "members"
