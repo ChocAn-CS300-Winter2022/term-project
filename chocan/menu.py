6a2f03862c7dcfe7734cce45dca5e5b111e8fe08
Alana Gilston <abluescarab@users.noreply.github.com>
2022-02-04 18:47:59 -0800

    Updated print statements, added docstrings

--- chocan/menu.py
+++ chocan/menu.py
@@ -4,16 +4,14 @@ enable fluid navigation through the ChocAn system
 '''
 
 from enum import Enum
-import enum
-
 
 
 #Class for the menu. Contains the functions for each menu that will be utilized
 #for the ChocAn system.
 class Menu:
-    
-    #enum the options to give visible description when being checked against other parts of the system.
-    class Menu_Enum(Enum):
+    #enum the options to give visible description when being checked against
+    #other parts of the system.
+    class MenuPage(Enum):
         FirstLoad = 1
         Login = 2
         ProviderMain = 3
@@ -21,28 +19,26 @@ class Menu:
 
     #Menu constructor - using page inspiried by Alana's teachings
     def __init__(self):
-        self.page = Menu.Menu_Enum.FirstLoad
+        """Create a new Menu instance."""
+        self.page = Menu.MenuPage.FirstLoad
 
-    #display the menu system
-    def menu_display(self):
-
+    def display(self):
+        """Display the menu system."""
         #TODO Testing input
-        print("Pick a number 1-4 - though we don't actually do anything with it yet")
+        print("Pick a number 1-4 - though we don't actually do anything with it"
+              " yet")
 
-        #TODO refer to the crash-course and design doc on what to put here... This could
-        #be cut smaller by combining the last elif into the provider main some how?
-        #it also doesn't hurt to just have double menu in manager I suppose?
-        '''if self.page == Menu.Menu_Enum.FirstLoad:
-            #TBD
-        elif self.page == Menu.Menu_Enum.Login:
-            #TBD
-        elif self.page == Menu.Menu_Enum.ProviderMain:
-            #TBD
-        elif self.page == Menu.Menu_Enum.ManagerMain:
-            #TBD'''
-        
+        #TODO refer to the crash-course and design doc on what to put here...
+        #This could be cut smaller by combining the last elif into the provider
+        #main some how? it also doesn't hurt to just have double menu in manager
+        #I suppose?
+        if self.page == Menu.MenuPage.FirstLoad:
+            print("first load")
+        elif self.page == Menu.MenuPage.Login:
+            print("login")
+        elif self.page == Menu.MenuPage.ProviderMain:
+            print("provider main")
+        elif self.page == Menu.MenuPage.ManagerMain:
+            print("manager main")
+
         return input("> ")
-
-if __name__ == "__main__":
-    menu = Menu()
-    menu.menu_display()
