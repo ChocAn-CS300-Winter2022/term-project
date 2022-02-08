5c78633c672d5d4fb1d22afa24bb057360be2ba5
Alana Gilston <abluescarab@users.noreply.github.com>
2022-02-08 04:12:46 -0800

    Implemented Report file; only need to override generate_report()

--- chocan/reports/provider_report.py
+++ chocan/reports/provider_report.py
@@ -2,8 +2,5 @@ from chocan.reports.report import Report
 
 
 class ProviderReport(Report):
-    def write(self):
-        pass
-
     def generate_report(self):
         pass
--- chocan/reports/report.py
+++ chocan/reports/report.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from datetime import datetime
+
 from chocan import utils
 
 
@@ -7,21 +10,24 @@ class Report:
         self.services = []
         self.report = ""
 
-        # TODO: Instead of overriding write() in each subclass, set self.folder
-        # to determine which folder it should go in?
-        # self.folder = ""
-
     def write(self):
         """Write the report to disk."""
-        self.report = self.generate_report()
+        self.generate_report()
 
-        # TODO: Write to disk
+        if not utils.check_file(self.path):
+            print(f"Could not write report for member with ID "
+                  f"{self.services[0].member.id} to disk.")
+            return
+
+        with open(self.path, 'w') as file:
+            file.write(self.report)
 
     def display(self):
         """Display the report in the terminal."""
         if not self.report:
             if utils.confirmation("No report found. Generate it?"):
                 self.generate_report()
+                print()
             else:
                 print("No report to display.")
                 return
@@ -31,5 +37,29 @@ class Report:
 
     def generate_report(self):
         """Generate the report text."""
-        # TODO: Generate the report
-        pass
+        self.report = ""
+        member = self.services[0].member
+        self.path = self.get_file(member)
+
+        self.report += f"{member.name} (ID: {member.id})\n" \
+                       f"{member.address}, {member.city}, {member.state} " \
+                       f"{member.zip_code}\n\n"
+
+        for i in range(0, len(self.services)):
+            service = self.services[i]
+
+            self.report += f"Date:     " \
+                           f"{service.date_provided.strftime('%Y-%m-%d')}\n" \
+                           f"Provider: {service.provider.name}\n" \
+                           f"Service:  {service.service_name}"
+
+            if i < (len(self.services) - 1):
+                self.report += "\n\n"
+
+    @staticmethod
+    def get_file(person):
+        """Get the file that the report should be written to."""
+        folder = "providers" if person.id.startswith("8") else "members"
+
+        return Path(".") / "reports" / folder / \
+               f"{datetime.now().date().strftime('%Y%m%d')}_{person.id}.txt"
