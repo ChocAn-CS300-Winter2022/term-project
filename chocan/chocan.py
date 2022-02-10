bd8cc6f7b055563e6c28335c3ea1370921160cba
Alana Gilston <abluescarab@users.noreply.github.com>
2022-02-09 22:08:39 -0800

    Added current_person; comments above menu items; returns to correct terminal

--- chocan/chocan.py
+++ chocan/chocan.py
@@ -9,6 +9,7 @@ from chocan.menu import Menu
 class ChocAn:
     def __init__(self):
         self.menu = Menu()
+        self.current_person = None
 
         path = Path(".") / "restricted" / "provider_directory.json"
 
@@ -27,7 +28,9 @@ class ChocAn:
         while not quit:
             command = self.menu.display()
 
+            # Login page
             if self.menu.page == Menu.MenuPage.LogIn:
+                # Ask for ID
                 if command == "1":
                     users = [file.stem for file in path.glob("*.json")]
                     id = input("Enter an ID: ")
@@ -36,68 +39,103 @@ class ChocAn:
                     if not (id in users and is_provider or id.startswith("9")):
                         print("Invalid ID. Please try again.")
                     else:
+                        with open(path / f"{id}.json", "r") as file:
+                            self.current_person = json.load(file)
+
                         self.menu.page = (Menu.MenuPage.ProviderTerminal
                             if is_provider else Menu.MenuPage.ManagerTerminal)
+                # Exit program
                 elif command == "0":
                     quit = True
                 else:
                     print("Invalid command. Please try again.")
+            # Provider terminal
             elif self.menu.page == Menu.MenuPage.ProviderTerminal:
+                # Manage services
                 if command == "1":
                     self.menu.page = Menu.MenuPage.Services
+                # Back to login
                 elif command == "0":
                     self.menu.page = Menu.MenuPage.LogIn
                 else:
                     print("Invalid command. Please try again.")
+            # Manager terminal
             elif self.menu.page == Menu.MenuPage.ManagerTerminal:
+                # Manage services
                 if command == "1":
                     self.menu.page = Menu.MenuPage.Services
+                # Manager users
                 elif command == "2":
                     self.menu.page = Menu.MenuPage.UserInformation
+                # Manage reports
                 elif command == "3":
                     self.menu.page = Menu.MenuPage.Reports
+                # Back to login page
                 elif command == "0":
                     self.menu.page = Menu.MenuPage.LogIn
                 else:
                     print("Invalid command. Please try again.")
+            # Manage services
             elif self.menu.page == Menu.MenuPage.Services:
+                # Show provider directory
                 if command == "1":
                     self.display_provider_directory()
+                # Add a service record
                 elif command == "2":
                     print("Add service record")
+                # Back to terminal
                 elif command == "0":
-                    # TODO: Determine whether provider or manager
-                    self.menu.page = Menu.MenuPage.ManagerTerminal
+                    if self.current_person["id"].startswith("8"):
+                        self.menu.page = Menu.MenuPage.ProviderTerminal
+                    else:
+                        self.menu.page = Menu.MenuPage.ManagerTerminal
                 else:
                     print("Invalid command. Please try again.")
+            # Manage users
             elif self.menu.page == Menu.MenuPage.UserInformation:
+                # Add member
                 if command == "1":
                     print("Add member")
+                # Remove member
                 elif command == "2":
                     print("Remove member")
+                # Modify member
                 elif command == "3":
                     print("Modify member")
+                # Add provider
                 elif command == "4":
                     print("Add provider")
+                # Remove provider
                 elif command == "5":
                     print("Remove provider")
+                # Modify provider
                 elif command == "6":
                     print("Modify provider")
+                # Back to terminal
                 elif command == "0":
-                    # TODO: Determine whether provider or manager
-                    self.menu.page = Menu.MenuPage.ManagerTerminal
+                    if self.current_person["id"].startswith("8"):
+                        self.menu.page = Menu.MenuPage.ProviderTerminal
+                    else:
+                        self.menu.page = Menu.MenuPage.ManagerTerminal
                 else:
                     print("Invalid command. Please try again.")
+            # Manage reports
             elif self.menu.page == Menu.MenuPage.Reports:
+                # Generate summary report
                 if command == "1":
                     print("Generate summary report")
+                # Generate member report
                 elif command == "2":
                     print("Generate member report")
+                # Generate provider report
                 elif command == "3":
                     print("Generate provider report")
+                # Back to terminal
                 elif command == "0":
-                    # TODO: Determine whether provider or manager
-                    self.menu.page = Menu.MenuPage.ManagerTerminal
+                    if self.current_person["id"].startswith("8"):
+                        self.menu.page = Menu.MenuPage.ProviderTerminal
+                    else:
+                        self.menu.page = Menu.MenuPage.ManagerTerminal
                 else:
                     print("Invalid command. Please try again.")
 
