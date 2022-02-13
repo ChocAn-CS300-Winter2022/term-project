c351021427191acc3f3636353c563740a8ec490a
Alana Gilston <abluescarab@users.noreply.github.com>
2022-02-12 20:17:26 -0800

    Disconnected SummaryReport from Report

--- chocan/reports/summary_report.py
+++ chocan/reports/summary_report.py
@@ -1,11 +1,14 @@
-from chocan.reports.report import Report
+from pathlib import Path
 
 
-class SummaryReport(Report):
+class SummaryReport:
     def __init__(self):
         self.providers = {}
 
     def write(self):
+        pass
+
+    def display(self):
         pass
 
     def generate_report(self):
